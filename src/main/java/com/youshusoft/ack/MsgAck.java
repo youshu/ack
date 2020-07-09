8931b26fc6c9408cf8a50a2c9bd58d5320623bcf
youshu <youshusoft@gmail.com>
2020-07-09 23:33:27 +0800

    方法注释

--- src/main/java/com/youshusoft/ack/MsgAck.java
+++ src/main/java/com/youshusoft/ack/MsgAck.java
@@ -4,11 +4,29 @@ package com.youshusoft.ack;
  * 消息响应确认器
  */
 public interface MsgAck {
+    /**
+     * 等待响应,指定超时时间
+     * @param msgId
+     * @param time
+     * @return
+     */
     boolean await(String msgId,int time);
 
+    /**
+     * 等待响应
+     * @param msgId
+     * @return
+     */
     boolean await(String msgId);
 
+    /**
+     * 响应通知
+     * @param msgId
+     */
     void signal(String msgId);
-
+    /**
+     * 响应通知
+     * @param msgId
+     */
     void signalAll(String msgId);
 }
